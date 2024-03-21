5dac45113b1ae16e3a6caaf4da40a160c4b771ba
Amarok <snowlyicewolf@outlook.com>
2024-03-21 14:52:51 +0800

    fix

--- src/main/java/club/someoneice/callablehorse/client/CallableHorseFabricClient.java
+++ src/main/java/club/someoneice/callablehorse/client/CallableHorseFabricClient.java
@@ -12,18 +12,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 import org.lwjgl.glfw.GLFW;
 
-/*
-This Source Code Form is subject to the terms of the Mozilla Public License, v2.0. If a copy of the MPL was not distributed with this file,
-You can obtain one at https://mozilla.org/MPL/2.0/.
-
-Alternatively, the contents of this file may be used under the terms of the GNU General Public License Version LGPL, as described below:
-This file is free software: you may copy, redistribute and/or modify it under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version LGPL of the License.
-This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
-*/
-
 public class CallableHorseFabricClient implements ClientModInitializer {
     public KeyMapping SET_HORSE = KeyBindingHelper.registerKeyBinding(new KeyMapping(keyStringKey("setHorse"), InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_P, keyStringKey("key." + CallableHorseFabric.MODID)));
     public KeyMapping CALL_HORSE = KeyBindingHelper.registerKeyBinding(new KeyMapping(keyStringKey("callHorse"), InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, keyStringKey("key." + CallableHorseFabric.MODID)));
